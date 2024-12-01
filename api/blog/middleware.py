cf52737fe51ecd566c228218a59a3251dfeed34f
Thupha41 <phatngo.040103@gmail.com>
2024-12-01 17:53:54 +0700

    Fix path in none secure path middleware

--- api/blog/middleware.py
+++ api/blog/middleware.py
@@ -12,7 +12,7 @@ NON_SECURE_PATHS = [
     r"/api/v1/auth/register",
     r"/api/v1/auth/login",
     r"/api/v1/blogs/posts", 
-    r"/api/v1/blogs/posts/\d+/details",  
+    r"/api/v1/blogs/posts/\d+",  
     r"/api/v1/blogs/posts/\d+/comments",  
     r"/api/v1/blogs/posts/\d+/images",
     r"/api/docs"  
